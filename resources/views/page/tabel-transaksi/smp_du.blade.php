6ee66e3c6aa7cfae05a9da857614d08142785d1c
dikaantoniputra <dikahanggara2@gmail.com>
2024-10-16 10:49:33 +0700

    update

--- resources/views/page/tabel-transaksi/smp_du.blade.php
+++ resources/views/page/tabel-transaksi/smp_du.blade.php
@@ -161,13 +161,7 @@
 
 
 @push('after-script')
-<script src="{{ asset('') }}/assets/libs/jquery/jquery.min.js"></script>
-    <script src="{{ asset('') }}/assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="{{ asset('') }}/assets/libs/simplebar/simplebar.min.js"></script>
-    <script src="{{ asset('') }}/assets/libs/node-waves/waves.min.js"></script>
-    <script src="{{ asset('') }}/assets/libs/waypoints/lib/jquery.waypoints.min.js"></script>
-    <script src="{{ asset('') }}/assets/libs/jquery.counterup/jquery.counterup.min.js"></script>
-    <script src="{{ asset('') }}/assets/libs/feather-icons/feather.min.js"></script>
+
 
 <!-- third party js -->
 <script src="{{ asset('') }}assets/libs/datatables.net/js/jquery.dataTables.min.js"></script>
